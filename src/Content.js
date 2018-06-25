dbc9f13957d77987a79dea89712b4815b42cb023
dmitry mineev <duke16@mail.ru>
2018-06-25 22:29:44 +0300

    state

--- src/Content.js
+++ src/Content.js
@@ -2,34 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Content extends React.Component {
-    render() {
-      const {activity} = this.props;
+  render() {
+    const {activity} = this.props;
+      <div className="content">
+      <div className="line"></div>
 
-        return (
-          <div className="content">
-          <div className="line"></div>
-
-        {/* Timeline item */}
-          {activity.map((activity) => {
-            <div className="item">
-              <div className="avatar">
-                <img
-                  alt={activity.text}
-                  src={activity.user.avatar} />
-                {activity.user.name}
-              </div>
-
-              <span className="time">
-                {activity.timestamp}
-              </span>
-              <p>{activity.text}</p>
-              <div className="commentCount">
-                {activity.comments.length}
-              </div>
-            </div>
-          )
-          })}
-        </div>
-      )
-    }
+      {/* Timeline item */}
+      {activity.map((activity) => {
+        <ActivityItem activity={activity} />
+      })}
+    </div>
+  }
 }
