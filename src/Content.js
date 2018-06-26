87cb6b244ede48105bfd3bbbfb498a22c1e23779
dmitry mineev <duke16@mail.ru>
2018-06-26 09:05:08 +0300

    component`s lifecycle

--- src/Content.js
+++ src/Content.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activity: []
+    }
+  }
+  componentWillMount() {
+    this.setState({activity: data})
+  }
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.requestRefresh !== this.props.requestRefresh) {
+      this.setState({loading: true}, this.updateData);
+    }
+  }
+
   render() {
     const {activity} = this.props;
       <div className="content">
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+class Footer extends React.Component {
+    render() {
+      return (
+        <div className='footer'>
+          {this.props.children}
+        </div>
+      )
+    }
+  }
+  
